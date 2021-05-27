ef14ab91dc1ecc0a6d954fc2367bcfccb4814f1a
Sripadh .M <42747902+sripadhse@users.noreply.github.com>
2021-05-27 10:41:55 +0530

    Add files via upload

---
+++ STDIN, Write to STDOUT.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+	  int num;
+
+    char str[15];
+
+    scanf("%d", &num);
+
+    num=num*2;
+
+    scanf("%s", str);          
+
+    printf("%d\n", num);
+
+    printf("%s", str);
+
+    
+
+    return 0;
+}
